f8bf11d88b66d75eb7a22632796983df60386c1b
Lucas Souza <lucasas@gmail.com>
2013-08-26 10:18:47 -0300

    including tomcat-solr service

--- tomcat-solr/recipes/default.rb
+++ tomcat-solr/recipes/default.rb
@@ -15,7 +15,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
+include_recipe "tomcat-solr::service"
+
 case node[:platform]
 when "debian", "ubuntu"
     package "tomcat7" do
